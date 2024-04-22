a591b35e78815f3caf3af66ea1754c9101a7a67c
diego97jaramillo <diego97jaramillo@gmail.com>
2024-04-21 23:31:08 -0500

    22 ejercicios hechos

--- src/js/main.js
+++ src/js/main.js
@@ -65,18 +65,106 @@ if((num1 + num2)**2 === (num1**2+2*num1*num2+num2**2)) {
     console.log('es un cuadrado perfecto');
 } else {'no es un cuadrado perfecto'}
 // 15.Determina si un número es un número de Fibonacci.
+let ultimo = 0;
+let penultimo = 0;
+let i = 0;
+let numeroFib = 0;
+let esFib = Number(prompt('Dame un numero: '));
+while (i < 101) {
+    if (i === 0) {
+        numeroFib = i;
+        i++
+    } else if (i === 1 && penultimo === 0) {
+        penultimo = i;
+        ultimo = i;
+        numeroFib = i;
+        i++
+    } else if (i === (ultimo + penultimo)) {
+        penultimo = ultimo;
+        ultimo = i;
+        numeroFib = i;
+        if (esFib === numeroFib) {
+            console.log('es fibonacci');
+            break
+        };
+        i++
+    } else {
+        i++
+    }
+};
 // 16.Verifica si un número es una potencia de 2.
+let esPot = Number(prompt('danos el numero: '))
+console.log(Math.log2(esPot));
 // 17.Determina si un número es un palíndromo.
+const cadena = 'oso';
+let cadenaPal = '';
+for(let i=0;i<cadena.length;i++) {
+
+    cadenaPal = cadena[i] + cadenaPal;
+}
+
+if (cadenaPal === cadena) {
+    console.log('es un palindromo');
+} else {
+    console.log('no es un palindromo');
+}
+
 // 18.Verifica si una cadena de texto contiene la palabra "JavaScript".
+const frase = 'estoy estudiando codigo en Javascript';
+console.log(frase.includes('Javascript'));
 // 19.Determina si una cadena tiene más de 10 caracteres.
+let cadena10char = prompt('ingrese su cadena: ');
+
+if (cadena10char.length > 10) {
+    console.log('tiene más de 10 caracteres');
+} else {
+    console.log('no tiene los suficientes');
+};
 // 20.Verifica si una cadena de texto está en minúsculas.
+let letras = "abcdefghijklmnñopqrstuvwxyz";
+let cadenaPropmt = prompt('escribe una palabra: ');
+let mensaje = '';
+for(let i = 0; i<cadenaPropmt.length; i++) {
+    if (letras.indexOf(cadenaPropmt.charAt(i),0)!==-1) {
+        mensaje = 'Si tiene una minúsculas';
+        break
+    } else {
+        mensaje = 'No tiene minúscula';
+    };
+};
+
+console.log(mensaje);
+
 // 21.Determina si una cadena de texto contiene al menos un número.
+const numeros1 = '0123456789'
+let cadenaPropmtNum = prompt('escribe una palabra: ');
+let mensajeNum = '';
+for(let i = 0; i<cadenaPropmtNum.length; i++) {
+    if (letras.indexOf(cadenaPropmtNum.charAt(i),0)!==-1) {
+        mensajeNum = 'Si tiene un numero';
+        break
+    } else {
+        mensajeNum = 'No tiene un numero';
+    };
+};
+console.log(mensajeNum);
 // 22.Verifica si una cadena de texto termina con un punto (.).
+const cadena = 'soy un loco.';
+if (cadena.charAt(cadena.length -1) === '.') {
+    console.log('Si termina con un .');
+} else {
+    console.log(cadena.charAt(cadena.length -1));
+}
+
+
 // 23.Determina si una cadena de texto es un pangrama (contiene todas
 // las letras del alfabeto).
+
+
 // 24.Verifica si un día de la semana es laborable (de lunes a viernes).
 // 25.Determina si un día es fin de semana (sábado o domingo).
 // 26.Verifica si un número representa un mes válido (del 1 al 12).
+
 // 28.Determina si una hora está en el rango de 9 AM a 6 PM.
 // 29.Verifica si una persona es mayor de 65 años o menor de 18 años.
 // 30.Determina si un triángulo es equilátero (tres lados iguales).
